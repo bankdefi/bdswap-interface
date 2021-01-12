7dbcad0c518697f6b1dd65aec4fcf525dc4e732a
Morpheus <morpheus.blockchain@gmail.com>
2021-01-12 11:16:34 +0800

    Update networkId supported.

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -27,7 +27,7 @@ export function getNetworkLibrary(): Web3Provider {
 }
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42, 128, 256]
+  supportedChainIds: [128, 256]
 })
 
 // mainnet only
